4e41bf7caec17c52b7d97f8abb436d9dee515d88
Andrew Cummings <agcummings@gmail.com>
2023-10-23 21:33:13 -0700

    some more tag colors

--- scripts/tagColors.js
+++ scripts/tagColors.js
@@ -1,9 +1,16 @@
 const GREEN_TAG = "greenTag";
 const BLUE_TAG = "blueTag";
+const RED_TAG = "redTag";
+const ORANGE_TAG = "orangeTag";
 const TAG_COLORS = {
     "MemCom": GREEN_TAG,
     "ED": GREEN_TAG,
     "BoD": GREEN_TAG,
     "Powers, Limitations and Availability": BLUE_TAG,
-    "Guidelines": BLUE_TAG
+    "Guidelines": BLUE_TAG,
+    "Robert's Rules": BLUE_TAG,
+    "Deposit": RED_TAG,
+    "Room and Board": RED_TAG,
+    "Office": ORANGE_TAG
 };
+

1a6e018bcdec6a376004db32d58b2a75e336025a
Andrew Cummings <agcummings@gmail.com>
2023-11-05 14:54:41 -0800

    two typos..

--- scripts/tagOrder.js
+++ scripts/tagOrder.js
@@ -21,7 +21,7 @@ const tagOrder = {
         "B. Chore Deficiency": {},
         "Bathroom Clean-up Obligations": {}
     },
-    "IV. Fine Debit, and Credit Procedure": {
+    "IV. Fine, Debit, and Credit Procedure": {
         "A. Fine and Credit Procedure": {},
         "B. Authority to Issue Fines, Debits": {},
         "C. Authority to Issue Credits and Credit": {},
